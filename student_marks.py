5268d1008badc510d22c6ede21d9ebba34b05ba7
Justine <mbilinyijustin14@gmail.com>
2025-07-28 12:33:46 +0300

    marks for 1 studnt

--- student_marks.py
+++ student_marks.py
@@ -1,1 +1,11 @@
 import numpy as np
+
+# Each row = marks for 1 student in [Math, English, Science]
+marks = np.array([
+    [75, 80, 72],
+    [60, 65, 70],
+    [90, 88, 95],
+    [55, 58, 60]
+])
+
+print("Marks:\n", marks)
